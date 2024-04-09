d3bf5a0c5e22cb2bb9fc70fca7c9185b1ca9847b
Ammar Ahmad <aa068445@gmail.com>
2024-04-09 15:02:50 +0500

    minor fixes with redux persistance

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
 import { persistReducer } from 'redux-persist';
 import userReducer from './features/users/userSlice'
 import authReducer from './features/auth/authSlice'
@@ -12,14 +13,13 @@ const reducers = combineReducers({
 });
 
 const persistConfig = {
-  key: 'root',
+  key: 'key',
   storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    reducer: persistedReducer
-  }
+  reducer: persistedReducer,
+  middleware: () => [thunk]
 })
